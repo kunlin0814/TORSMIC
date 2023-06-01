670359608ad297a395929a58982ac1d08be181d3
kunlin0814 <42413453+kunlin0814@users.noreply.github.com>
2023-06-01 16:39:53 -0400

    Update pipeline_ml_mutation_filtering.sh

--- pipeline_ml_mutation_filtering.sh
+++ pipeline_ml_mutation_filtering.sh
@@ -8,35 +8,36 @@
 #SBATCH --output=Step1_extract_somatic.%j.out    # Standard output log
 #SBATCH --error=Step1_extract_somatic.%j.err     # Standard error log
 
-#### The pathes that need to be changed
+#### The paths that need to be changed
 package_location='/home/kh31516/kh31516_Lab_Share_script/IdentifiyNeoAntigene/TORSMIC' ## the location where you put the package
 new_add_data='/scratch/kh31516/UGA/UGA_OSA_somatic_germline.txt.gz'                  ## the location of the table which you concat all of the results from all the samples
-pipeline_out_file_name='/scratch/kh31516/UGA/UGA_OSA_pipeline_filtering.txt' ## the output file from the pipeline without maching learning prediction
-ml_out_file_name='/scratch/kh31516/UGA/UGA_OSA_ml_filtering.txt' ## the output file from the pipeline with maching learning prediction
+pipeline_out_file_name='/scratch/kh31516/UGA/UGA_OSA_pipeline_filtering.txt' ## the output file from the pipeline without machine learning prediction
+ml_out_file_name='/scratch/kh31516/UGA/UGA_OSA_ml_filtering.txt' ## the output file from the pipeline with machine learning prediction
 ####
 
-scripts_location=${package_location}/scripts
-data_source_location=${package_location}/data_source
+scripts_location="${package_location}/scripts"
+data_source_location="${package_location}/data_source"
 
 ml R/4.0.0-foss-2019b
+
 # package_location <- as.character(args[1])
 # new_data_file <- as.character(args[2])
 # out_file_name <- as.character(args[3])
 
-Rscript --vanilla ${scripts_location}/Sapelo2_add_new_data_whole_pipeline.R \
-${package_location} \
-${new_add_data} \
-${pipeline_out_file_name} 
+Rscript --vanilla "${scripts_location}/Sapelo2_add_new_data_whole_pipeline.R" \
+"${package_location}" \
+"${new_add_data}" \
+"${pipeline_out_file_name}"
 
 ml Miniconda3/4.9.2
 source activate /home/kh31516/myenv/py38
 
-cd $scripts_location
+cd "$scripts_location"
 
 # model_folder = sys.argv[1]
 # data_test_file = sys.argv[2]
 # final_output = sys.argv[3]
-python ${scripts_location}/Sapelo2_ML_test_pipeline_data.py \
-${data_source_location} \
-${pipeline_out_file_name}  \
-${ml_out_file_name}
+python "${scripts_location}/Sapelo2_ML_test_pipeline_data.py" \
+"${data_source_location}" \
+"${pipeline_out_file_name}" \
+"${ml_out_file_name}"
